2d2f7beadf645f58778aa0bf729383a26955a899
galodelia <gmdelia@mail.austral.edu.ar>
2025-03-31 18:36:18 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,16 @@
 def change():
     expense = 23.75
     money = 100
+    changeint = 76
+    cent = 25
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print()
+    print("Vuelto")
+    print()
+    print("Pesos:")
+    print(changeint)
+    print("Centavos:")
+    print(cent)
